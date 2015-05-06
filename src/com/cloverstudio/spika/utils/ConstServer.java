266704fab52e8605f6b44bcda432d06b4abd1184
timeriverdesign <timeriverdesign@192.168.1.63>
2015-05-06 14:20:30 +0800

    message

--- src/com/cloverstudio/spika/utils/ConstServer.java
+++ src/com/cloverstudio/spika/utils/ConstServer.java
@@ -3,8 +3,7 @@ package com.cloverstudio.spika.utils;
 public class ConstServer {
 
 	/* Change to your server adress */
-	public static final String BASE_URL                	= "http://officialapi.spikaapp.com";
-	public static final String INFORMATION_URL			= "http://officialapi.spikaapp.com/page/information/";
-	public static final String LIST_SERVERS_URL         = BASE_URL + "/" + Const.API_FOLDER + "servers";
-	
+	public static final String BASE_URL = "http://spika.trcorp.cho88.com:80/wwwroot";
+	public static final String INFORMATION_URL	= "http://spika.trcorp.cho88.com:80/wwwroot/page/information/";
+	public static final String LIST_SERVERS_URL = BASE_URL + "/" + Const.API_FOLDER + "servers";
 }
