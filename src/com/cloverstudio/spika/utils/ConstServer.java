87918939d2874d5805efc2e897e8fa170cc191a5
trd <trd@192.168.1.23>
2015-05-08 09:06:39 -0700

    testing

--- src/com/cloverstudio/spika/utils/ConstServer.java
+++ src/com/cloverstudio/spika/utils/ConstServer.java
@@ -5,6 +5,6 @@ public class ConstServer {
 	/* Change to your server adress */
 	public static final String BASE_URL                	= "http://officialapi.spikaapp.com";
 	public static final String INFORMATION_URL			= "http://officialapi.spikaapp.com/page/information/";
-	public static final String LIST_SERVERS_URL         = BASE_URL + "/" + Const.API_FOLDER + "servers";
+	public static final String LIST_SERVERS_URL         = BASE_URL + "/" + Const.API_FOLDER + "servers"; //testing
 	
 }
